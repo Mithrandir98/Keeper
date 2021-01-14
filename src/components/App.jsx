7ea586ec3f55f9e52dbeb88f406e440cfbe940cb
Ruthwik H Param <aspire1998@gmail.com>
2021-01-14 10:28:23 +0700

    Change in app.jsx
    Changed the map function in note from normal function call to arrow function.

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -4,21 +4,18 @@ import Note from "../components/Note";
 import notes from "../notes";
 import Footer from "../components/Footer";
 
-function newNote(note) {
-    return (
-        <Note 
-        key={note.key}
-        title={note.title}        
-        content={note.content}
-        />
-    );
-}
 
 function App() {
     return (
         <div>
         <Header />
-        {notes.map(newNote)}
+        {notes.map(note => (
+        <Note 
+            key={note.key}
+            title={note.title}        
+            content={note.content}
+        />
+        ))}
         <Footer />
         </div>
     );
